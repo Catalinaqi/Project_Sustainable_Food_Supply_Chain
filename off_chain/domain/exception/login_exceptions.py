316433e3c2026a69c56b563c4a94b9ae1920d938
Ft2801 <157906143+Ft2801@users.noreply.github.com>
2025-05-17 11:06:28 +0200

    sistemato un errore di login

--- off_chain/domain/exception/login_exceptions.py
+++ off_chain/domain/exception/login_exceptions.py
@@ -1,7 +1,8 @@
 class LoginFailExetion(Exception):
     """Eccezione sollevata quando la quantità disponibile è insufficiente."""
-    def __init__(self,):
-        super().__init__(f"Login fallito: credenziali errate")
+    def __init__(self, msg=None):
+        message = f"Login fallito: {msg}" if msg else "Login fallito: credenziali errate"
+        super().__init__(message)
 
 class ToManyTryLogEXcepition(Exception):
     """Eccezione sollevata quando la quantità disponibile è insufficiente."""
